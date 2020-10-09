d510a343574fdf49ed0774a36281cf1d3d46213e
David Hutchison <dhutchis68516@gmail.com>
2020-10-09 14:35:33 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.integer :price
       t.float :size
       t.string :image_url
-
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
